14eadde312286686e84a246c1881e028eb5b0715
geekyparth2004 <goelparth20049@gmail.com>
2026-04-10 22:37:20 +0530

    Right Click Disabled

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,7 +1,20 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        const handleContextMenu = (e: MouseEvent) => {
+            e.preventDefault();
+        };
+
+        document.addEventListener("contextmenu", handleContextMenu);
+        
+        return () => {
+            document.removeEventListener("contextmenu", handleContextMenu);
+        };
+    }, []);
+
     return <SessionProvider>{children}</SessionProvider>;
 }
